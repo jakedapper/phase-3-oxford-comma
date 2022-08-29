c70d3196044e2f387c73ca51795203771f5d3970
jake dapper <jakedapper@gmail.com>
2022-08-29 11:25:13 -0500

    commit tree 8e004845152a3a67e77d484258973f5a1675031e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
+require "pry" 
 
+def oxford_comma(array)
+  comma_array = array.each do |string|
+    string << ","
+  end
+  and_index = comma_array.length - 1
+  and_array = comma_array.splice(and_index, "and")
+  and_array.join(" ")
 end
+
+ok =["fiddleheads", "okra", "kohlrabi"]
+puts oxford_comma(ok)
